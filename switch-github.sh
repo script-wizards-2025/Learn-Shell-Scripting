03892693316ea8fa09ed210ab9b49c3923a12bcf
radhagovind-daas2k25 <ritikguptarocks2002@gmail.com>
2025-02-20 13:22:59 +0000

    updated switch-github.sh and added .gitignore

--- switch-github.sh
+++ switch-github.sh
@@ -1,21 +1,14 @@
 #!/bin/bash 
-
-
 Github_username=$1
 
 if [ -z "${Github_username}" ];then
     echo "Usage: $0 <Github-username>"
     exit 1
 fi
-# Github_Accounts[username]=<ssh alias> <git email> 
-declare -A Github_Accounts=(
-["rkdaas"]="github-first gupta.ritik2002@gmail.com"
-["onlydevops-2k25"]="github-second onlygatestudy@gmail.com"
-["radhagovind-daas2k25"]="github-third ritikgupta2002@gmail.com"
-)
 
 # get account details and check whether github user exists or not 
-account_info="${Github_Accounts[${Github_username}]}"
+account_info=$(grep -w ${Github_username} github_accounts.conf)
+
 if [ -z "${account_info}" ];then
     echo "Error: Account ${Github_username} not found ! "
     exit 1 
@@ -23,14 +16,14 @@ fi
 
 function Switchuser()
 {
-IFS=' ' read -r alias email <<< ${Github_Accounts[$Github_username]} 
-echo "Switching to GitHub account: $Github_username"
+IFS=' ' read -r username alias email <<< "${account_info}"
+echo "Switching to GitHub account: $username"
 # Switch SSH key
 ssh -T "git@$alias"
 git config user.name "${Github_username}"
 git config user.email "${email}"
-echo "📝 Active Git Config: User = $(git config --get user.name), Email = $(git config --get user.email)"
-echo "✅ Successfully switched to GitHub account: $Github_username"
+echo "Active Git Config: User = $(git config --get user.name), Email = $(git config --get user.email)"
+echo "Successfully switched to GitHub account: $username"
 }
 
 # main script 
